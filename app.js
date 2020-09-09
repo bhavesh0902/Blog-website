a830532c654c02d3373b3ce0c76207fa83636b35
bhavesh0902 <60576636+bhavesh0902@users.noreply.github.com>
2020-09-09 22:59:56 +0530

    content update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose=require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017/blogDB",{useNewUrlParser: true});
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "This is the home page of blog website. You can add new blogs from /compose route. You can view the added blog by clicking readmore. Also this is connected to a mongodb, thus your blogs will not be removed after reopening the website. It also uses the ejs templating for various pages and for header and footer. For css, it uses bootstrap classes and components, also with some plain css";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -15,8 +15,6 @@ const blogSchema={
   title: String,
   body: String
 };
-
-// let posts=[]
 
 const Blog=mongoose.model("Blog", blogSchema);
 const app = express();
@@ -37,7 +35,6 @@ app.get("/",function(req, res){
     });
 
   };
-
 });
 });
 
@@ -57,8 +54,6 @@ app.get("/compose",function(req, res){
 });
 
 app.get('/posts/:id', function (req, res) {
-  // res.send(req.params)
-  // console.log(req.params.name);
 
   const toFind=req.params.id;
 
@@ -76,7 +71,6 @@ app.post("/compose",function(req, res){
     title: req.body.postTitle,
     body: req.body.postBody
   });
-  // posts.push(data);
 
   blog.save(function(err){
     if(!err){
